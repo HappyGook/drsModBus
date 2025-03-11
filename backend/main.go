c42696d8d9dd378a546da4fbceebbb44b53b2c98
Oleg <oleg.radzhabov@schnoor-ins.com>
2025-03-11 12:48:12 +0100

    first communication steps

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"gith
+	"log"
 	"net/http"
+	"time"
 )
 
 type Input struct {
@@ -12,7 +15,6 @@ type Input struct {
 
 func main() {
 	r := gin.Default()
-
 	r.POST("/api/submit", handleSubmit)
 
 	err := r.Run(":8080")
@@ -32,5 +34,34 @@ func handleSubmit(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"data": data})
 	fmt.Println("Data! :) ", data)
+
+}
+
+func drsCom() {
+	ip := "192.168.1.100" // PLACEHOLDER !!!!
+	port := "502"
+
+	handler := modbus.NewTCPClientHandler(fmt.Sprintf("%s:%s", ip, port))
+	handler.Timeout = 10 * time.Second
+
+	err := handler.Connect()
+	if err != nil {
+		log.Fatal("Connection error: ", err)
+	}
+	defer func(handler *modbus.TCPClientHandler) {
+		err := handler.Close()
+		if err != nil {
+
+		}
+	}(handler)
+
+	client := modbus.NewClient(handler)
+
+	results, err := client.ReadHoldingRegisters(0x00, 2)
+	if err != nil {
+		log.Fatal("Reading failed: ", err)
+	}
+
+	fmt.Printf("Register values: %v\n\n", results)
 
 }
